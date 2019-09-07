45e98c8c27ee6adc888bbacc73b5ccd67756107e
AaronBalogh <34430508+AaronBalogh@users.noreply.github.com>
2019-09-07 00:08:20 -0400

    Add files via upload

---
+++ Lab1/drawCircle.py
@@ -0,0 +1,11 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+myTurtle.circle(50)
+
+myTurtle1 = turtle.Turtle()
+myTurtle.circle(70)
+
+myTurtle2 = turtle.Turtle()
+myTurtle.circle(90)
+turtle.getscreen().root.mainloop()
---
+++ Lab1/loops.py
@@ -0,0 +1,9 @@
+for i in range(0,11,2):
+    print(i)
+
+for x in range(20,14, -1):
+    print(x)
+
+for i in range(0,101,2):
+    print(i)
+
